dd753e0d46dcedd8b0de67157523fd382ec176de
Richard Johnny Rober Wallnerström <riwajr@gmail.com>
2023-09-26 20:01:46 +0200

    Add project files.

---
+++ AssetTracker/Asset.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AssetTracker
+{
+    public class Asset
+    {
+        public Asset(string type, string brand, string model, string office, double price, double currency, DateTime purchaseDate)
+        {
+            Type = type;
+            Brand = brand;
+            Model = model;
+            Office = office;
+            Price = price;
+            Currency = currency;
+            PurchaseDate = purchaseDate;
+        }
+
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        
+        public string Model { get; set; }
+        public string Office { get; set; }
+        public double Price { get; set; }
+        public double Currency { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public void RunOption(char keyInput)
+        {
+
+        }
+        public static void AddAsset(List<Asset> assetList)
+        {
+            
+        }
+    }
+}
---
+++ AssetTracker/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CC = System.ConsoleColor;
+
+namespace AssetTracker
+{
+    internal class Program
+    {
+        internal static void PrintOptions()
+        {
+            Print("\nPress a desired key to select an option.\n\n", CC.DarkGreen);
+            Print("Press \"A\" to add an Asset\n" +
+                "Press \"D\" to display Assets\n" +
+                "Press \"Q\" to quit\n");
+        }
+        internal static void Print(string text, CC fgColor = CC.White, CC bgColor = CC.Black)
+        {
+            Console.ForegroundColor = fgColor;
+            Console.BackgroundColor = bgColor;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+        internal static void PrintLine(string text, CC fgColor = CC.White, CC bgColor = CC.Black)
+        {
+            Console.ForegroundColor = fgColor;
+            Console.BackgroundColor = bgColor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        internal static void Main()
+        {
+            List<Asset> assetList = new List<Asset>();
+            Print("-------------------------------\n|", CC.DarkBlue);
+            Print(" Welcome to the AssetTracker ", CC.DarkYellow);
+            Print("|\n-------------------------------\n", CC.DarkBlue);
+            while (true)
+            {
+                PrintOptions();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true); //Hide the key from the console
+                char keyChar = char.ToLower(keyInfo.KeyChar);
+                if (keyChar == 'q')                                 // Quit
+                {
+                    PrintLine("\nExiting application...\n", CC.Red);
+                    break;
+                }
+                else if (keyChar == 'a')
+                {
+                    Asset newAsset;
+                    Console.ReadLine();
+                    Asset.AddAsset(assetList);
+
+                }
+                else
+                {
+                    PrintLine($"\n{keyChar} is not a valid option!\n", CC.Red);
+                }
+            }   
+
+
+            
+
+        }
+    }
+   
+}
+
+
