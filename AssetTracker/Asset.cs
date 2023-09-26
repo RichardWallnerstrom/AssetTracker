8f5c1246ea9e55e4c8539067e82d4392e0e11a45
Richard Johnny Rober Wallnerström <riwajr@gmail.com>
2023-09-26 21:48:22 +0200

    Added Asset.AddAsset() Added Asset.DisplayAssets()

--- AssetTracker/Asset.cs
+++ AssetTracker/Asset.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CC = System.ConsoleColor;
 
 
 namespace AssetTracker
 {
     public class Asset
     {
-        public Asset(string type, string brand, string model, string office, double price, double currency, DateTime purchaseDate)
+        public Asset(string type, string brand, string model, string location, double price, DateTime purchaseDate)
         {
             Type = type;
             Brand = brand;
             Model = model;
-            Office = office;
+            Location = location;
             Price = price;
-            Currency = currency;
             PurchaseDate = purchaseDate;
         }
 
@@ -24,9 +24,9 @@ namespace AssetTracker
         public string Brand { get; set; }
         
         public string Model { get; set; }
-        public string Office { get; set; }
+        public string Location { get; set; }
         public double Price { get; set; }
-        public double Currency { get; set; }
+        public string Currency { get; set; }
 
         public DateTime PurchaseDate { get; set; }
 
@@ -36,7 +36,45 @@ namespace AssetTracker
         }
         public static void AddAsset(List<Asset> assetList)
         {
-            
+            Program.Print("\n What type of asset is this?  ", CC.Cyan);
+            string type = Console.ReadLine().ToLower();
+            while (type != "computer" && type != "phone" && type != "car")
+            {
+                Program.Print($"\n {type} is not a valid asset. We currently track computers, phones and cars.  \n", CC.Red);
+                Program.Print("\n What type of asset is this?  ", CC.Cyan);
+                type = Console.ReadLine().ToLower();
+
+            }
+            Program.Print("\n What brand is it?  ", CC.Cyan);                        string brand = Console.ReadLine();
+            Program.Print("\n Which model is it?  ", CC.Cyan);                       string model = Console.ReadLine();
+            Program.Print("\n Which country is the office located in?  ", CC.Cyan);  string location = Console.ReadLine();
+            Program.Print("\n How much did it cost in US$?  ", CC.Cyan);             double price = Convert.ToDouble(Console.ReadLine());
+            Program.Print("\n Enter the purchase date (yyyy-MM-dd):  ", CC.Cyan);    DateTime purchaseDate;
+
+            while (true)
+            {
+                if (DateTime.TryParse(Console.ReadLine(), out purchaseDate))
+                {
+                    Asset newAsset = new Asset(type, brand, model, location, price, purchaseDate);
+                    assetList.Add(newAsset);
+                    Program.Print("\n  " + char.ToUpper(newAsset.Brand[0]) + newAsset.Brand.Substring(1) + " " 
+                        + newAsset.Type + " added to the office in "    
+                        + char.ToUpper(newAsset.Location[0]) + newAsset.Location.Substring(1) 
+                        + ".\n", CC.DarkGreen);
+                    break;
+                }
+                else
+                {
+                    Program.Print("\n Invalid date format. ", CC.DarkRed);
+                }
+            }
+        }
+        public static void DisplayAssets(List<Asset> assetList)
+        {
+            foreach (Asset asset in assetList)
+            {
+                Program.Print($"\n {asset.Type.PadRight(20)}{asset.Brand.PadRight(20)}{asset.Model.PadRight(20)}{asset.Location.PadRight(20)}{asset.Price.ToString().PadRight(20)}{asset.PurchaseDate.ToString().PadRight(20)}  ");
+            }
         }
     }
 }
--- AssetTracker/Program.cs
+++ AssetTracker/Program.cs
@@ -11,10 +11,10 @@ namespace AssetTracker
     {
         internal static void PrintOptions()
         {
-            Print("\nPress a desired key to select an option.\n\n", CC.DarkGreen);
-            Print("Press \"A\" to add an Asset\n" +
-                "Press \"D\" to display Assets\n" +
-                "Press \"Q\" to quit\n");
+            Print("\n Press a desired key to select an option.\n\n", CC.DarkGreen);
+            Print(" Press \"A\" to add an Asset\n" +
+                " Press \"D\" to display Assets\n" +
+                " Press \"Q\" to quit\n", CC.Cyan);
         }
         internal static void Print(string text, CC fgColor = CC.White, CC bgColor = CC.Black)
         {
@@ -23,39 +23,35 @@ namespace AssetTracker
             Console.Write(text);
             Console.ResetColor();
         }
-        internal static void PrintLine(string text, CC fgColor = CC.White, CC bgColor = CC.Black)
-        {
-            Console.ForegroundColor = fgColor;
-            Console.BackgroundColor = bgColor;
-            Console.WriteLine(text);
-            Console.ResetColor();
-        }
         internal static void Main()
         {
             List<Asset> assetList = new List<Asset>();
-            Print("-------------------------------\n|", CC.DarkBlue);
+            Print(" ---------------------------------\n | ", CC.DarkBlue);
             Print(" Welcome to the AssetTracker ", CC.DarkYellow);
-            Print("|\n-------------------------------\n", CC.DarkBlue);
+            Print(" | \n ---------------------------------\n", CC.DarkBlue);
             while (true)
             {
                 PrintOptions();
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true); //Hide the key from the console
                 char keyChar = char.ToLower(keyInfo.KeyChar);
-                if (keyChar == 'q')                                 // Quit
+                if (keyChar == 'q')         // Quit
                 {
-                    PrintLine("\nExiting application...\n", CC.Red);
+                    Print("\nExiting application...\n", CC.Red);
                     break;
                 }
-                else if (keyChar == 'a')
+                else if (keyChar == 'a')    // Add assets
                 {
-                    Asset newAsset;
-                    Console.ReadLine();
-                    Asset.AddAsset(assetList);
-
+                     Asset.AddAsset(assetList);
+                }
+                else if (keyChar == 'd')    // Display assets
+                {
+                    Print("\n     TYPE".PadRight(20) + "BRAND".PadRight(20) + "MODEL".PadRight(20) + "LOCATION".PadRight(20) + "PRICE".PadRight(20) + "PURCHASE DATE".PadRight(20) +
+                        "\n    -------------------------------------------------------------------------------------------------------------\n", CC.DarkYellow);
+                    Asset.DisplayAssets(assetList);
                 }
                 else
                 {
-                    PrintLine($"\n{keyChar} is not a valid option!\n", CC.Red);
+                    Print($"\n{keyChar} is not a valid option!\n", CC.Red);
                 }
             }   
 
